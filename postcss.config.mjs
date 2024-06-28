644c6de728c93347d3b2707783b3f38fcda0453c
Krishna Bardoliya <krishnabardoliya@gmail.com>
2024-06-28 19:45:14 +0530

    fixed

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,20 +1,6 @@
 module.exports = {
-  plugins: [
-    "tailwindcss",
-    process.env.NODE_ENV === "production"
-      ? [
-          "@fullhuman/postcss-purgecss",
-          {
-            content: [
-              './pages/**/*.{js,ts,jsx,tsx,mdx}',
-              './components/**/*.{js,ts,jsx,tsx,mdx}',
-              './app/**/*.{js,ts,jsx,tsx,mdx}',
-            ],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        ]
-      : undefined, // Removido o valor 'false'
-    "postcss-preset-env",
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
