30bbc1679ffb6eb1c775f2ea95ecd18470b549e2
Krishna Bardoliya <krishnabardoliya@gmail.com>
2024-06-28 18:27:30 +0530

    updated tailwind config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
-import tailwind from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-module.exports = {
-	plugins: [
-		// Some plugins, like postcss-nested, need to run before Tailwind
-		tailwind(),
-		// But others, like autoprefixer, need to run after
-		autoprefixer()
-	]
+const config = {
+  plugins: {
+  },
 };
+
+export default config;
