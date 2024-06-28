5e3f77d85b0269fa911a45610ff4386b8691d08e
Krishna Bardoliya <krishnabardoliya@gmail.com>
2024-06-28 19:04:34 +0530

    fixed issue

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,20 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    "tailwindcss",
+    process.env.NODE_ENV === "production"
+      ? [
+          "@fullhuman/postcss-purgecss",
+          {
+            content: [
+              './pages/**/*.{js,ts,jsx,tsx,mdx}',
+              './components/**/*.{js,ts,jsx,tsx,mdx}',
+              './app/**/*.{js,ts,jsx,tsx,mdx}',
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        ]
+      : undefined, // Removido o valor 'false'
+    "postcss-preset-env",
+  ],
 };
