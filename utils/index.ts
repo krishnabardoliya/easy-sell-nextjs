0add2a2e963c4da66596d3daf7fe971528d80cfc
Krishna Bardoliya <35481724+krishnabardoliya@users.noreply.github.com>
2024-06-28 14:21:05 +0530

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -10,7 +10,7 @@ export const getCanonicalUrl = () => {
     return `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/easy-sell-product-images/${imageUrl}`;
   };
 
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+  const supabaseUrl = "https://jlzvktrtmclopylirnmh.supabase.co"
+  const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpsenZrdHJ0bWNsb3B5bGlybm1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkzMTI1OTgsImV4cCI6MjAzNDg4ODU5OH0.BcbYZKDO18iKSV4_kGfU_huz-4Sinc27C6gsMzj3EtE"
   export const supabase = createClient(supabaseUrl, supabaseAnonKey)
   
