15d3f44eb6f8c806bf6d0aa58f15bd0692a939fe
Krishna Bardoliya <krishnabardoliya@gmail.com>
2024-06-28 18:59:17 +0530

    fixed issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  enabled: process.env.NODE_ENV === "production",
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
